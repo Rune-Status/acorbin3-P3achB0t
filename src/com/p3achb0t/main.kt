45eff3efa8a01afc7738811a0f4de14468811350
zaksmithcomputing <44407558+zaksmithcomputing@users.noreply.github.com>
2020-04-23 18:32:04 +0100

    adding private files

--- src/com/p3achb0t/main.kt
+++ src/com/p3achb0t/main.kt
@@ -1,38 +1,51 @@
 package com.p3achb0t
 
 import com.formdev.flatlaf.FlatDarkLaf
-import com.p3achb0t.client.ui.GameWindow
-import com.p3achb0t.client.ui.setup
-import com.p3achb0t.scripts_debug.paint_debug.PaintDebug
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import com.p3achb0t.analyser.Analyser
+import com.p3achb0t.analyser.runestar.RuneStarAnalyzer
+import com.p3achb0t.client.configs.Constants
+import com.p3achb0t.client.configs.GlobalStructs
+import com.p3achb0t.client.loader.Loader
+import com.p3achb0t.client.util.Util
+import com.p3achb0t.client.ux.BotManager
 import java.awt.Font
+import java.io.File
+import java.nio.file.Paths
+import java.util.jar.JarFile
 import javax.swing.UIManager
-import javax.swing.UIManager.setLookAndFeel
 import javax.swing.plaf.FontUIResource
 
 
 object Main {
 
-    var validationKey = PaintDebug.key
+    //var validationKey = PaintDebug.key
     @JvmStatic
     fun main(args: Array<String>) {
 
         var getNextKey = false
         args.iterator().forEach {
             if(getNextKey){
-                validationKey = PaintDebug.key
+                //validationKey = PaintDebug.key
             }
             if(it == "-key"){
                 getNextKey = true
             }
             println(it)
         }
-        println("validation key: $validationKey")
+        //println("validation key: $validationKey")
 
-        setup()
-        setLookAndFeel(FlatDarkLaf())
+        // Todo: Temp solution
+        update()
+        lookAndFeel()
+        val botManager = BotManager()
+        GlobalStructs.botManager = botManager
+        botManager.startAccounts()
+        botManager.updateCache()
+        botManager.startScripts()
+    }
+
+    private fun lookAndFeel() {
+        UIManager.setLookAndFeel(FlatDarkLaf())
         for ((key) in UIManager.getDefaults()) {
             val value = UIManager.get(key)
             if (value != null && value is FontUIResource) {
@@ -41,7 +54,31 @@ object Main {
                 UIManager.put(key, f)
             }
         }
-        val g = GameWindow()
-        CoroutineScope(Dispatchers.Default).launch { g.run() }
+    }
+
+    // TODO: Move this somewhere
+    private fun update() {
+        System.setProperty("user.home", "cache")
+        Util.createDirIfNotExist(Paths.get(Constants.APPLICATION_CACHE_DIR, Constants.JARS_DIR).toString())
+        // check applet revision
+        val revision = Util.checkClientRevision(Constants.REVISION, 3000)
+        if (!revision) {
+            error("New revision, need to update hooks")
+        }
+
+        //Check to see if we have an injected JAR for the specific revision
+        //Handle case where missing injection Jar
+        //Download new Gamepack
+        //Run analyzer and inject new gamepack
+        if(!File("${Constants.APPLICATION_CACHE_DIR}/${Constants.INJECTED_JAR_NAME}").exists()) {
+            val loader = Loader()
+            val gamePackWithPath = loader.run()
+            val gamePackJar = JarFile(gamePackWithPath)
+            println("Using $gamePackWithPath")
+            val runeStar = RuneStarAnalyzer()
+            runeStar.loadHooks()
+            runeStar.parseJar(gamePackJar)
+            Analyser().createInjectedJar(gamePackJar, runeStar)
+        }
     }
 }
